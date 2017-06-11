78a6d79a2b132a1365c49709edaf44e11f9f2bb9
IAN SIMPSON <ianconradsimpson@gmail.com>
2017-06-11 12:11:45 +0100

    added random first ball and buttons in index.html

--- interface.js
+++ interface.js
@@ -2,5 +2,9 @@ $(document).ready(function() {
   var bowling = new BowlingGame();
   $('#score').text(bowling.score);
 
+  $('#bowl_one').on('click', function() {
+    bowling.bowl_one();
+    $('#score').text(bowling.score);
+  })
 
 });
--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -2,13 +2,13 @@ function BowlingGame() {
   this.score = 0;
   this.pins = 10;
 
-
-BowlingGame.prototype.getCurrentScore = function() {
-  return this.score;
-};
+// BowlingGame.prototype.getCurrentScore = function() {
+//   return this.score;
+// };
 
 BowlingGame.prototype.bowl_one = function () {
-  (this.score += 7)
+  var scoreOne =  Math.floor(Math.random() * 10) + 1;
+  (this.score += scoreOne);
 };
 
 BowlingGame.prototype.bowl_two = function () {
