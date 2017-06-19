777c75702181366d7fef664e2104458745865c9b
IAN SIMPSON <ianconradsimpson@gmail.com>
2017-06-19 09:27:48 +0100

    Added random throws to ball 1 and ball two

--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -7,18 +7,22 @@ function BowlingGame() {
   var scorecard = {};
 
   BowlingGame.prototype.bowlFirst = function (pins) {
-    var score =  0 //Math.floor(Math.random() * 10) + 1;
+    var score =  Math.floor(Math.random() * 10) + 1;
       this.scoreOne =+ score
 
     console.log(this.scoreOne)
   };
 
   BowlingGame.prototype.bowlSecond = function (pins) {
-    var score =  0 //Math.floor(Math.random() * 10) + 1;
+    var score =  Math.floor(Math.random() * (10 - this.scoreOne)) + 1;
       this.scoreTwo =+ score
 
     console.log(this.scoreTwo)
   };
+
+  BowlingGame.prototype.randomCalc = function() {
+
+  }
 
 
 
